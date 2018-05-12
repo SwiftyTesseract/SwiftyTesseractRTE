1efea67046f740ceab3940d8c6e899cec3f37188
Steven Sherry <steven.r.sherry@gmail.com>
2018-05-12 16:12:56 -0500

    feat: added additional cases to RecognitionReliability

--- SwiftyTesseractRTE/Classes/ConcreteTypes/RecognitionReliability.swift
+++ SwiftyTesseractRTE/Classes/ConcreteTypes/RecognitionReliability.swift
@@ -8,6 +8,10 @@
 
 /// The level of reliability required to assure OCR accuracy. The higher the reliability, the longer it will take to return results
 public enum RecognitionReliability: Int {
+  /// Immediately returns the first result
+  case raw = 1
+  /// Requires the last two frames return the same results
+  case tentative = 2
   /// Requires the last three frames return the same results
   case verifiable = 3
   /// Requires the last four frames return the same results
